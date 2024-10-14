796213599d813a2f816161ff6c0ad4c2a93ab41f
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-15 00:24:24 +0300

    Add navigation listeners

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,52 @@
 const slider = document.querySelector("#mainSlider");
-const slides = document.querySelectorAll("#mainSlider > .main-slider--slides > .slide");
+const slides = document.querySelectorAll("#mainSlider .main-slider--slides > .slide");
+const miniatures = document.querySelectorAll("#mainSlider .main-slider--miniatures > .miniature");
+const leftBtn = document.querySelector("#mainSlider .left-btn");
+const rightBtn = document.querySelector("#mainSlider .right-btn");
 
 let slide = 0;
 let firstStartTimeout = 7000;
-let changeSlideTimeout = 3000;
+let changeSlideTimeout = 5000;
+
+leftBtn.addEventListener("click", function(){console.log('left')});
+rightBtn.addEventListener("click", function(){console.log('right')});
+
+for(let i = 0; i < miniatures.length; i++){
+    miniatures[i].addEventListener('click', function(e){
+        console.log(this.getAttribute('data-slide'));
+    });
+}
 
 setTimeout(changeSlide, firstStartTimeout);
 
 function changeSlide(){
+    increment();
+
+    slides[slide].classList.add('active');
+    miniatures[slide].classList.add('active');
+
+    for(let i = 0; i < slides.length; i++){
+        if(i != slide){
+            slides[i].classList.remove('active');
+            miniatures[i].classList.remove('active');
+        }
+    }
+
+    setTimeout(changeSlide, changeSlideTimeout);
+}
+
+function increment(){
     slide++;
 
     if(slide > (slides.length - 1)){
         slide = 0;
     }
+}
 
-    slides[slide].classList.add('show');
+function decrement(){
+    slide--;
 
-    for(let i = 0; i < slides.length; i++){
-        if(i != slide){
-            slides[i].classList.remove('show');
-        }
+    if(slide < 0){
+        slide = slides.length - 1;
     }
-
-    setTimeout(changeSlide, changeSlideTimeout);
 }

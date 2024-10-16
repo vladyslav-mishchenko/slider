efe099fe98323bd325b535b5bd1d1e93fdb03891
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-16 21:34:15 +0300

    Add js class for use more than one independent sliders

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,122 +1,154 @@
-const slider = document.querySelector("#mainSlider");
-const slides = document.querySelectorAll("#mainSlider .main-slider--slides > .slide");
-const miniatures = document.querySelectorAll("#mainSlider .main-slider--miniatures > .miniature");
-const leftBtn = document.querySelector("#mainSlider .left-btn");
-const rightBtn = document.querySelector("#mainSlider .right-btn");
-const center = document.querySelector("#mainSlider .center");
+class Elements {
+    constructor(slider) {
+        this.slides = this.els(slider + ' .main-slider--slides > .slide');
+        this.miniatures = this.els(slider + ' .main-slider--miniatures > .miniature');
+        this.leftBtn = this.el(slider + ' .left-btn');
+        this.rightBtn = this.el(slider + ' .right-btn');
+        this.center = this.el(slider + ' .center');
+    }
 
-let slide = startFromSlide();
-let firstStartTimeout = 7000;
-let changeSlideTimeout = 5000;
-let setTimeoutId = null;
-let touchStartX = null;
+    el(element) {
+        return document.querySelector(element);
+    }
 
-start();
-
-function start(){
-    setTimeoutId = setTimeout(run, firstStartTimeout);
+    els(elements) {
+        return document.querySelectorAll(elements);
+    }
 }
 
-function stop(){
-    clearTimeout(setTimeoutId);
-}
+export class Slider extends Elements {
+    firstStartTimeout = 7000;
+    changeSlideTimeout = 5000;
+    slide = this.startFromSlide();
+    setTimeoutId = null;
+    touchStartX = null;
 
-function run(){
-    animationOn();
-    increment();
-    changeSlide();
-    setTimeoutId = setTimeout(run, changeSlideTimeout);
-}
+    constructor(parameters) {
+        super(parameters.slider);
+        this.events();
+    }
 
-function next(){
-    stop();
-    animationOff();
-    increment();
-    changeSlide();
-    start();
-}
+    start() {
+        this.setTimeoutId = setTimeout(() => this.run(), this.firstStartTimeout);
+    }
 
-function prev(){
-    stop();
-    animationOff();
-    decrement();
-    changeSlide();
-    start();
-}
+    stop() {
+        clearTimeout(this.setTimeoutId);
+    }
 
-function changeSlide(){
-    slides[slide].classList.add('active');
-    miniatures[slide].classList.add('active');
+    run() {
+        this.slider();
+        this.setTimeoutId = setTimeout(() => this.run(), this.changeSlideTimeout);
+    }
 
-    for(let i = 0; i < slides.length; i++){
-        if(i != slide){
-            slides[i].classList.remove('active');
-            miniatures[i].classList.remove('active');
+    slider() {
+        this.animationOn();
+        this.increment();
+        this.changeSlide();
+    }
+
+    prev() {
+        this.stop();
+        this.animationOff();
+        this.decrement();
+        this.changeSlide();
+        this.start();
+    }
+
+    next() {
+        this.stop();
+        this.animationOff();
+        this.increment();
+        this.changeSlide();
+        this.start();
+    }
+
+    increment() {
+        this.slide++;
+
+        if (this.slide > (this.slides.length - 1)) {
+            this.slide = 0;
+        }
+    }
+
+    decrement() {
+        this.slide--;
+
+        if (this.slide < 0) {
+            this.slide = (this.slides.length - 1);
+        }
+    }
+
+    animationOn() {
+        for (let i = 0; i < this.slides.length; i++) {
+            this.slides[i].classList.remove('na');
+        }
+    }
+
+    animationOff() {
+        for (let i = 0; i < this.slides.length; i++) {
+            this.slides[i].classList.add('na');
+        }
+    }
+
+    changeSlide() {
+        this.slides[this.slide].classList.add('active');
+        this.miniatures[this.slide].classList.add('active');
+
+        for (let i = 0; i < this.slides.length; i++) {
+            if (i != this.slide) {
+                this.slides[i].classList.remove('active');
+                this.miniatures[i].classList.remove('active');
+            }
+        }
+    }
+
+    changeSlideById(e) {
+        this.stop();
+        this.animationOff();
+
+        this.slide = Number(e.srcElement.getAttribute('data-slide'));
+
+        this.changeSlide();
+        this.start();
+    }
+
+    swipeStart(e) {
+        this.touchStartX = e.touches[0].clientX;
+    }
+
+    swipeEnd(e) {
+        if (this.touchStartX > e.changedTouches[0].clientX) {
+            this.prev();
+        } else {
+            this.next();
+        }
+    }
+
+    startFromSlide() {
+        for (let i = 0; i < this.slides.length; i++) {
+            if (this.slides[i].classList.contains('active')) {
+                return i;
+            }
+        }
+    }
+
+    events() {
+        if (this.leftBtn != null) {
+            this.leftBtn.addEventListener('click', () => this.prev());
+        }
+
+        if (this.rightBtn != null) {
+            this.rightBtn.addEventListener('click', () => this.next());
+        }
+
+        if (this.center != null) {
+            this.center.addEventListener('touchstart', (e) => this.swipeStart(e));
+            this.center.addEventListener('touchend', (e) => this.swipeEnd(e));
+        }
+
+        for (let i = 0; i < this.miniatures.length; i++) {
+            this.miniatures[i].addEventListener('click', (e) => this.changeSlideById(e));
         }
     }
 }
-
-function increment(){
-    slide++;
-
-    if(slide > (slides.length - 1)){
-        slide = 0;
-    }
-}
-
-function decrement(){
-    slide--;
-
-    if(slide < 0){
-        slide = (slides.length - 1);
-    }
-}
-
-function animationOff(){
-    for(let i = 0; i < slides.length; i++){
-        slides[i].classList.add('na');
-    }
-}
-
-function animationOn(){
-    for(let i = 0; i < slides.length; i++){
-        slides[i].classList.remove('na');
-    }
-}
-
-function startFromSlide(){
-    for(let i = 0; i < slides.length; i++){
-        if(slides[i].classList.contains('active')){
-            return i;
-        }
-    }
-}
-
-for(let i = 0; i < miniatures.length; i++){
-    miniatures[i].addEventListener('click', function(){
-        stop();
-        animationOff();
-
-        slide = this.getAttribute('data-slide');
-
-        changeSlide();
-        start();
-    });
-}
-
-function swipeStart(e){
-    touchStartX = e.touches[0].clientX;
-}
-
-function swipeEnd(e){
-    if(touchStartX > e.changedTouches[0].clientX){
-        prev();
-    }else{
-        next();
-    }
-}
-
-leftBtn.addEventListener("click", prev);
-rightBtn.addEventListener("click", next);
-center.addEventListener("touchstart", swipeStart)
-center.addEventListener("touchend", swipeEnd);
--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@
 // import './style.scss';
 
 // js
-import './js/slider';
+// import './js/slider';
+
+import { Slider as Slider } from "./js/slider";
+
+document.addEventListener('DOMContentLoaded', function(){
+    new Slider({slider: "#sld1"}).start();
+    new Slider({slider: "#sld2"}).start();
+    new Slider({slider: "#sld3"}).start();
+});
